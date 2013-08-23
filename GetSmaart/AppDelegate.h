add38e69fca40860968b60b5b945fd3a12b6f3b0
arkmon <arkmon@ymail.com>
2013-08-23 21:20:11 +0100

    Initial Commit

---
+++ GetSmaart/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GetSmaart
+//
+//  Created by Arkadiusz Dowejko on 23/08/2013.
+//  Copyright (c) 2013 SmaartPublishing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GetSmaart/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  GetSmaart
+//
+//  Created by Arkadiusz Dowejko on 23/08/2013.
+//  Copyright (c) 2013 SmaartPublishing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
